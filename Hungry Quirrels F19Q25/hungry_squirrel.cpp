cbdf21d652149ef8c0cb07dbfe9c262588d25639
Josh Hong <joshsw@umich.edu>
2025-12-08 21:35:30 -0500

    Initial commit

---
+++ Hungry Quirrels F19Q25/hungry_squirrel.cpp
@@ -0,0 +1,39 @@
+/* 
+ * hungry_squirrel.cpp 
+ * Univeristy of Michigan, Ann Arbor
+ * EECS 281 F19E2 Q25.
+ * SUBMIT ONLY THIS FILE TO GRADESCOPE.
+ */
+
+// Common #includes for convience.
+// No need to use them. 
+// Remove any of them if you want.
+#include <algorithm>
+#include <cassert>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+#include "hungry_squirrel.h"
+
+using namespace std;
+
+int get_max_nuts(std::vector<std::vector<int>>& diag, int nrow, int ncol) {
+  // TODO
+  return 0;
+}
